f98b9d5780a6cbd2a574dd516b0a1077b5226459
Andry <andry.kovalenko@gmail.com>
2018-06-10 20:58:25 +0300

    Revert "no"
    This reverts commit 2b03df458a455350387194fbea41c4e21cf3caf0.

--- SeaBattle/src/Ship.java
+++ SeaBattle/src/Ship.java
@@ -1,118 +1,27 @@
 
-
 public class Ship {
-	
-	private int deckNum;
-	private int health;
-	private int Xcor;//cordinats of first field
-	private int Ycor;
-	private boolean horizontal;
-	
-	
-	Ship(){	
+	int deckNum;
+	int health;
+	int Xcor;//cordinats of first field
+	int Ycor;
+	boolean horizontal;
+	Ship(){}
+	Ship(int deck,int x,int y,boolean horizontal) {
+		deckNum=deck;
+		health=deck;
+		Xcor=x-1;
+		Ycor=y-1;
+		this.horizontal=horizontal;
 	}
-	
-	
-	Ship(int x,int y,int numOfDeck,boolean alighment) {
-		this.Xcor=x;
-		this.Ycor=y;
-		this.deckNum=numOfDeck;
-		this.health = numOfDeck;
-		this.horizontal=alighment;
-	}
-	
-	
-	public boolean isKilled(){	
+	public boolean isKilled(){
+		
 			if(health>0)
 				return false;
+		
 		return true;
 	}
-	
-	
 	public void shoot() {
 		health--;
 	}
 
-
-	/**
-	 * @return the health
-	 */
-	public int getHealth() {
-		return health;
-	}
-
-
-	/**
-	 * @param health the health to set
-	 */
-	public void setHealth(int health) {
-		this.health = health;
-	}
-
-
-	/**
-	 * @return the xcor
-	 */
-	public int getXcor() {
-		return Xcor;
-	}
-
-
-	/**
-	 * @param xcor the xcor to set
-	 */
-	public void setXcor(int xcor) {
-		Xcor = xcor;
-	}
-
-
-	/**
-	 * @return the ycor
-	 */
-	public int getYcor() {
-		return Ycor;
-	}
-
-
-	/**
-	 * @param ycor the ycor to set
-	 */
-	public void setYcor(int ycor) {
-		Ycor = ycor;
-	}
-
-
-	/**
-	 * @return the horizontal
-	 */
-	public boolean isHorizontal() {
-		return horizontal;
-	}
-
-
-	/**
-	 * @param horizontal the horizontal to set
-	 */
-	public void setHorizontal(boolean horizontal) {
-		this.horizontal = horizontal;
-	}
-
-
-	/**
-	 * @return the deckNum
-	 */
-	public int getDeckNum() {
-		return deckNum;
-	}
-
-
-	/**
-	 * @param deckNum the deckNum to set
-	 */
-	public void setDeckNum(int deckNum) {
-		this.deckNum = deckNum;
-	}
-
-	
-	
 }
